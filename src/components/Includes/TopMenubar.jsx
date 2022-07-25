51a554f0e3704508925e496b57773c3f98b99bb0
mahalakshmiyml <lakshmiyml@gmail.com>
2022-07-25 12:54:45 +0530

    werfwerwe

--- src/components/Includes/TopMenubar.jsx
+++ src/components/Includes/TopMenubar.jsx
@@ -19,19 +19,19 @@ const TopMenubar = () => {
               <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
               <Nav.Link as={Link} to="/careers">Careers</Nav.Link>
               <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item as={Link} to="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown> */}
+                {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
